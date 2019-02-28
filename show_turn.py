4fc54d0d85996cdfffd1d4359ecf3dfc9c46b765
Robin Saxifrage <robin@stonebreakerdesigns.com>
2019-02-28 15:25:14 -0800

    [dev] fix show turn

--- show_turn.py
+++ show_turn.py
@@ -1,10 +1,7 @@
 import re
 import sys
 
-fn, turn = sys.argv[1:3]
-offs = 1
-if len(sys.argv) == 4:
-	offs = int(sys.argv[3])
+fn, turn = sys.argv[1:]
 
 with open(fn) as f:
 	log = f.read()
@@ -13,4 +10,4 @@ found = list()
 for match in re.finditer(r'begin turn %s\n.*?\nmove .*?\n'%turn, log, re.DOTALL):
 	found.append(match)
 
-print(found[-offs].group(0))
+print('\n\n#########\n\n'.join(found))
